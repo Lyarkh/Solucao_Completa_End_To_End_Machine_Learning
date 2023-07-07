64673a79673673f4fa550c230c4107bd954a04f1
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-06 22:16:11 -0300

    feat: carregando o modelo treinado

--- api/app.py
+++ api/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import joblib
 
 
 app = Flask(__name__)
+# Carregando modelo treinado para a predição
+modelo = joblib.load('data/modells/modelo_random_florest_v1.pkl')
+
 
 @app.route('/predicao/<parametro>', methods=['GET'])
 def root(parametro):
-    return {'online': f'{parametro}'}
+
+    predicao = modelo.predict()
 
 
 if __name__ == '__main__':
