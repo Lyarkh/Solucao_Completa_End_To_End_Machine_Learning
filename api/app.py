b7b4120dec551a75507c0b62a43be8b7a51fa791
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-06 22:11:11 -0300

    feat: testando conexão api

--- api/app.py
+++ api/app.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/predicao')
+def root():
+    return {'online': 'funcionando'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
