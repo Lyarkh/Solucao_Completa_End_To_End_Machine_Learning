96581cc282353a574082e563e74bb248746fb40c
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-07 21:00:13 -0300

    feat: fazendo tratativa dos parametros para retorno da predição

--- api/app.py
+++ api/app.py
@@ -8,11 +8,13 @@ modelo = joblib.load('data/modells/modelo_random_florest_v1.pkl')
 
 
 @app.route('/predicao/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>', methods=['GET'])
-def root(area, rooms,bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
+def root(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
+    lista_de_parametros = [area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax]
+    lista_de_parametros_tratado = [float(x) for x in lista_de_parametros]
 
     try:
-        predicao = modelo.predict()
-        return {'valor_aluguel': predicao}
+        predicao = modelo.predict([lista_de_parametros_tratado])
+        return {'valor_aluguel': str(predicao)}
     except:
         return {'aviso': 'erro ao fazer a predicao'}
 
