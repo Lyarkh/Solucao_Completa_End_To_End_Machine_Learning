1ea798c0a2404c108964c832211742726e36e8f6
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-08 21:00:24 -0300

    feat: criando conexão com o banco de dados do app

--- api/app.py
+++ api/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import joblib
+import sqlite3
 
 
 app = Flask(__name__)
 # Carregando modelo treinado para a predição
 modelo = joblib.load('data/modells/modelo_random_florest_v1.pkl')
 
+conexao_db = sqlite3.connect('Banco_dados_API.db')
+cursor = conexao_db.cursor()
 
 @app.route('/predicao/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>', methods=['GET'])
 def root(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
