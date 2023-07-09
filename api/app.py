554ec179965008507ec4b83bb145c82ba87ea172
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-08 21:14:09 -0300

    fix: corrigindo bugs aplicação banco de dados

--- api/app.py
+++ api/app.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 # Carregando modelo treinado para a predição
 modelo = joblib.load('data/modells/modelo_random_florest_v1.pkl')
 
-conexao_db = sqlite3.connect('Banco_dados_API.db')
-cursor = conexao_db.cursor()
 
 @app.route('/predicao/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>', methods=['GET'])
 def root(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
@@ -20,19 +18,27 @@ def root(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, p
 
     try:
         predicao = modelo.predict([lista_de_parametros_tratado])
-        lista_de_parametros.append(predicao)
+        lista_de_parametros_tratado.append(str(predicao))
+
+        input_ = ''
+        for valor in lista_de_parametros_tratado:
+            input_ = input_ + ';' + str(valor)
 
         fim = datetime.now()
         processamento = fim - inicio
 
         insert_dados = f'''
             INSERT INTO Log_APi (Inputs, Inicio, Fim, Processamento) VALUES
-            ({str(lista_de_parametros_tratado)}, {inicio}, {fim}, {processamento})
+            ({input_}, {inicio}, {fim}, {processamento})
         '''
 
+
+        conexao_db = sqlite3.connect('Banco_dados_API.db')
+        cursor = conexao_db.cursor()
         cursor.execute(insert_dados)
         conexao_db.commit()
-        
+        cursor.close()
+
         return {'valor_aluguel': str(predicao)}
     except:
         return {'aviso': 'erro ao fazer a predicao'}
