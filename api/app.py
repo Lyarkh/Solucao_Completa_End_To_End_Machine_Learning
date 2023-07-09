820347cffd9799808b078dd783dd544d9ab90ea2
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-07-08 21:05:54 -0300

    feat: fazendo o insert dos dados no banco de dados

--- api/app.py
+++ api/app.py
@@ -13,6 +13,8 @@ cursor = conexao_db.cursor()
 
 @app.route('/predicao/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>', methods=['GET'])
 def root(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
+    inicio = datetime.now()
+
     lista_de_parametros = [area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax]
     lista_de_parametros_tratado = [float(x) for x in lista_de_parametros]
 
@@ -20,10 +22,17 @@ def root(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, p
         predicao = modelo.predict([lista_de_parametros_tratado])
         lista_de_parametros.append(predicao)
 
-        insert_dados_ = f'''
+        fim = datetime.now()
+        processamento = fim - inicio
+
+        insert_dados = f'''
             INSERT INTO Log_APi (Inputs, Inicio, Fim, Processamento) VALUES
-            ({str(lista_de_parametros_tratado)}, {}, {}, {})
+            ({str(lista_de_parametros_tratado)}, {inicio}, {fim}, {processamento})
         '''
+
+        cursor.execute(insert_dados)
+        conexao_db.commit()
+        
         return {'valor_aluguel': str(predicao)}
     except:
         return {'aviso': 'erro ao fazer a predicao'}
